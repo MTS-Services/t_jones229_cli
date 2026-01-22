cecc7383ba151b0ae1db44b997ed3055417b4bfe
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2026-01-22 10:59:14 +0600

    update home page and list-booking page

--- src/components/Boat-list/BoatListHero.tsx
+++ src/components/Boat-list/BoatListHero.tsx
@@ -1,14 +1,16 @@
-import banner from "@/assets/banner.png";
+import banner from "@/assets/fishing-rods.avif";
 import board from "@/assets/boart.svg";
 import ReUseAbleBanner from "../common/ReUseAbleBanner";
 
 export default function BoatListHero() {
   return (
-    <div className="">
+    <div className="!max-w-7xl mx-auto !rounded-2xl my-16">
       <ReUseAbleBanner
         title="List your business on FishingTripper – Connect with anglers from around the world."
         description="Join thousands of charter captains and guides connecting with anglers worldwide."
         backgroundImage={banner.src}
+        backgroundImageClassName="!rounded-2xl"
+        className={"!bg-black/50"}
         boardImage={board.src}
         button={true}
         buttonTitle="Click here to get started!"
