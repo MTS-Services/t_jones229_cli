543cbae0a8db674854274a65735615cd853ed632
Nayem Islam <gangsn490@gmail.com>
2026-03-13 18:56:21 +0600

    feat: extend TripsBookProps type with additional properties for trip and boat details

--- src/types/tripsTypes.ts
+++ src/types/tripsTypes.ts
@@ -6,11 +6,22 @@ export type TripsBookProps = {
   trip: {
     tripName: string;
     id: any;
+    duration?: string;
+    departureTime?: string;
+    price?: number;
+    tripDays?: string[];
   };
   boat: {
     photos: { url: string }[];
     captain: any;
+    name?: string;
+    boatType?: string;
+    capacity?: number;
+    meetingPoint?: { city?: string; state?: string }[];
   };
   id: string;
   userId?: string;
+  groupSize?: number;
+  payFirst?: number;
+  payDue?: number;
 };
